4603e70459f1c0294b836284cfd24c94b83d26e5
Martin J. Klöckner <martin.cachari@gmail.com>
2020-10-27 10:53:11 -0300

    lines, words, and characters counter

---
+++ lwc.c
@@ -0,0 +1,36 @@
+/*	count lines, words, and characters in input	*/
+
+
+#include <stdio.h>
+
+#define	IN	1	/* inside a word	*/
+#define	OUT	0	/* outside a word	*/
+
+int main(void) {
+
+	int c, nl, nw, nc, state;
+
+	state = OUT;
+	nl = nw = nc = 0;
+
+	while((c = getchar()) != EOF) {
+		if(c != '\n') {
+			++nc;
+		}	else {
+			++nl;
+		}
+
+		if((c == ' ') || (c == '\n') || (c == '\t')) {
+			state = OUT;
+		}	else if(state == OUT) {
+			state = IN;
+			++nw;
+		}
+
+
+	}
+
+	printf("lines: %d, words: %d, chars: %d\n", nl, nw, nc);
+
+	return 0;
+}
