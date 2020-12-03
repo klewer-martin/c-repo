38474909a99d29efb03002231a495578a2b7a34c
Martin J. Klöckner <martin.cachari@gmail.com>
2020-12-03 20:54:51 -0300

    Added more exercices (mostly unfinished)

--- 95.11/guia03/ex26.c
+++ 95.11/guia03/ex26.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define MAX_LEN 100
-
+#define ERR_MAX_STR_LEN "Err: the entered number is too big"
 
 
 int main ( void ) {
@@ -16,12 +16,11 @@ int main ( void ) {
 	}
 
 //  This part couts the lengh of the first string
-//	and removes the new line character;
+//	and replaces the new line character for a blank space;
     for(i = 0; str1[i] != '\0'; i++)
 		if(str1[i] == '\n')
-			str1[i] = str1[i + 1];
-
-	printf("str1 length = %d\n", i);
+			str1[i] = ' ';
+			str1[i + 1] = '\0';
 
     if(fgets(str2, MAX_LEN, stdin) == NULL) {
         return 1;
@@ -32,30 +31,28 @@ int main ( void ) {
 	}
 
     num = atoi(buffer);
-	printf("str1 = %s\n", str1);
-	printf("str2 = %s\n", str2);
-	printf("num = %d\n", num);
 
 //  This part checks if the input number is bigger than
-//  the length of the string;
+//  the length of the first string;
     if(num > i) {
+		fprintf(stderr, ERR_MAX_STR_LEN"\n");
         return 1;
 	}
 
+
+//	Removes the new line chracter from string two;
 	for(i = 0; str2[i] != '\0'; i++)
 		if(str2[i] == '\n')
 			str2[i] = '\0';
 
     for(i = 0, j = 0; str2[j] != '\0'; i++) {
-        if(i <= num) {
+        if((i <= num) && (str1[i] != '\0')) {
             dest[i] = str1[i];
 		} else { 
             dest[i] = str2[j];
             j++;
 		}
 	}
-//	printf("%d\n", i);
-//	printf("%d\n", j);
-//	printf("%s\n", dest);
+	printf("%s\n", dest);
     return 0;
 }
---
+++ 95.11/guia03/ex29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX_LEN 20
+
+int main (void) {
+
+	int i, num, vector[MAX_LEN];
+	char buffer[MAX_LEN];
+
+	
+
+	for(i = 0; i < MAX_LEN; i++)
+		printf("%d\n", vector[i]);
+
+	return 0;
+}
